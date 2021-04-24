c154229db108238cac277bfeb98a4ef288bc06d6
goshva <sha.egor@gmail.com>
2021-04-24 23:03:26 +0300

    fix prices

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -469,7 +469,7 @@
                                     <div class="Product__body">
                                         <div class="Product__price uk-visible@m">
                                             <div class="Product__priceText" uk-tooltip="title: Цена при обмене старого аккумулятора; pos: top-left">от {{ $product->trade_price }} ₽</div>
-                                            <div class="Product__priceFrom"  uk-tooltip="title: Цена без обмена; pos: top-left">{{ $product->price  - ($product->price*8)/100 }}₽</div>
+                                            <div class="Product__priceFrom"  uk-tooltip="title: Цена без обмена; pos: top-left">{{ $product->price }}₽</div>
                                         </div>
 
                                         <div class="Product__description">
