02a35ba0f658ed0e88293f9719305a87bb34a653
goshva <sha.egor@gmail.com>
2021-04-26 11:57:29 +0300

    rusian serch2

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -614,7 +614,7 @@
 <script src="/js/uikit.min.js"></script>
 <script src="/js/uikit-icons.min.js"></script>
 <!-- My JavaScript -->
-<script src="/js/dz3.js"></script>
+<script src="/js/dz4.js"></script>
 <script src="/js/AsideBlock__checkboxInput.js"></script>
 <script src="/js/Catalog.js"></script>
 @yield('scripts')
